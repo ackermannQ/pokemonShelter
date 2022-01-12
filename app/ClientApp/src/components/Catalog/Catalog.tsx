2e661f5fbfcb27ffa9c32527ffd980b463970763
ackermannQ <ackermann.quentin@gmail.com>
2022-01-12 18:54:10 +0100

    Styling the catalog title

--- app/ClientApp/src/components/Catalog/Catalog.tsx
+++ app/ClientApp/src/components/Catalog/Catalog.tsx
@@ -1,4 +1,5 @@
 import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { CircularProgress, Typography } from '@mui/material';
 import { AxiosRequestConfig } from 'axios';
 import React from 'react';
@@ -7,7 +8,15 @@ import { getAllProducts } from './httpRepository';
 import { IProduct } from './IProduct';
 import ProductList from './ProductList';
 
+const useStyles = makeStyles((theme) => ({
+    title: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(3, 0, 6),
+    },
+}));
+
 export default function Catalog() {
+    const classes = useStyles();
     const [products, setProducts] = React.useState<IProduct[]>([]);
     const [loading, setLoading] = React.useState(true);
 
@@ -18,8 +27,11 @@ export default function Catalog() {
             .finally(() => setLoading(false));
     }, []);
 
+
     return (<>
-        <Typography variant='h3' sx={{ marginBottom: 10 }}>Our little friends</Typography>
+        <Typography className={classes.title} component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            Our little friends
+        </Typography>
         <Container>
             {loading ? <CircularProgress color="inherit" /> : <ProductList products={products} />}
         </Container>
