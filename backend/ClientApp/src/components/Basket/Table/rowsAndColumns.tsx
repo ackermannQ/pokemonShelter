57ee2d73e98d5137cb32d380eb380028b5da0849
ackermannQ <ackermann.quentin@gmail.com>
2022-01-22 18:24:52 +0100

    Renaming exported functions and adding documentation

--- backend/ClientApp/src/components/Basket/Table/rowsAndColumns.tsx
+++ backend/ClientApp/src/components/Basket/Table/rowsAndColumns.tsx
@@ -1,9 +1,12 @@
 import { GridColDef } from '@mui/x-data-grid';
 
 import DeleteButton from '../../DeleteButton';
-import { RemoveItem } from '../httpRepository';
 import { IBasket } from '../interfaces';
 
+/**
+ * Build the rows for the datagrid displaying the basket
+ * @param {IBasket | null} basket The basket to get the different data to display is passed in argument
+ */
 export default function buildRows(basket: IBasket | null) {
     return basket?.items?.map(item => {
 
@@ -19,7 +22,11 @@ export default function buildRows(basket: IBasket | null) {
     });
 }
 
-export function buildColumns(): GridColDef[] {
+/**
+ * Build the columns for the datagrid displaying the basket
+ * @param {(productId: number) => void} handleRemoveItem Callback to remove the item using the api and also using the context
+ */
+export function buildColumns(handleRemoveItem: (productId: number) => void): GridColDef[] {
 
     return [{ field: 'id', headerName: 'ProductId', width: 100 },
     { field: 'name', headerName: 'Name', width: 150 },
@@ -27,10 +34,16 @@ export function buildColumns(): GridColDef[] {
     { field: 'price', headerName: 'Price', width: 120 },
     { field: 'pictureUrl', headerName: '', width: 100, renderCell: (params) => <img style={{ height: 50, backgroundSize: 'contain' }} src={params.value} alt="pokemon" /> },
     { field: 'quantity', headerName: 'How many?', width: 100 },
-    { field: 'action', headerName: '', width: 100, renderCell: (params) => <DeleteButton onDelete={() => RemoveItem(params.value)} /> },
+    {
+        field: 'action', headerName: '', width: 100, renderCell: (params) => <DeleteButton onDelete={() => handleRemoveItem(params.value)} />
+    },
     ];
 }
 
+/**
+ * Calculate the total of the basket using the basket items' price
+ * @param {IBasket | null} basket
+ */
 export function getTotal(basket: IBasket | null) {
 
     return basket?.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
--- backend/ClientApp/src/components/Basket/httpRepository.tsx
+++ backend/ClientApp/src/components/Basket/httpRepository.tsx
@@ -2,15 +2,27 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
+/**
+ * Get the basket
+ */
 export function getBasket() {
     return axios.get("/api/basket");
 }
 
-export function AddItem(productId: number, quantity: number = 1) {
+/**
+ * Add a product to the basket
+ * @param {number} productId Id corresponding to the product to add
+ * @param {number} quantity Quantity of the product to add, 1 by default
+ */
+export function add(productId: number, quantity: number = 1) {
     return axios.post(`/api/basket?productId=${productId}&quantity=${quantity}`, {});
 }
 
-
-export function RemoveItem(productId: number, quantity: number = 1) {
+/**
+ * Remove a product from the basket
+ * @param {number} productId Id corresponding to the product to add
+ * @param {number} quantity Quantity of the product to add, 1 by default
+ */
+export function remove(productId: number, quantity: number = 1) {
     return axios.delete(`/api/basket?productId=${productId}&quantity=${quantity}`);
 }
